4e92253202c364fe9b8283178a88583d124f9f0f
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-05-26 00:08:42 +0200

    balances block

--- src/blocks/User/Balance.jsx
+++ src/blocks/User/Balance.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import icon from '../../img/icon.png';
+import TICKET_icon from '../../img/TICKET.png';
+import XP_icon from '../../img/XP.png';
+import TON_icon from '../../img/TON.png';
 import './balance.css';
 
 const Balance = () => {
   return (
     <div className='balances'>
       <div className='xp balance'>
-        <img src={icon} alt="icon of this type balance" className='icon balance_icon' />
+        <img src={XP_icon} alt="icon of this type balance" className='icon balance_icon' />
         <p className='balance_value'>1234</p>
       </div>
 
       <div className='dividing-line'></div>
       
       <div className='ton balance'>
-        <img src={icon} alt="icon of this type balance" className='icon balance_icon' />
+        <img src={TON_icon} alt="icon of this type balance" className='icon balance_icon' />
         <p className='balance_value'>1234</p>
       </div>
       
       <div className='dividing-line'></div>
       
       <div className='tickets balance'>
-        <img src={icon} alt="icon of this type balance" className='icon balance_icon' />
+        <img src={TICKET_icon} alt="icon of this type balance" className='icon balance_icon' />
         <p className='balance_value'>1234</p>
       </div>
     </div>
