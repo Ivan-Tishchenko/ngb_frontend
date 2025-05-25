5e17cbc3e2d0f6901bdb86e2f4fb1d60ae2651a4
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-05-26 01:10:54 +0200

    navigation v0.5
    add navigation, but styles and animations not added

--- src/blocks/Nav.jsx
+++ src/blocks/Nav.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import icon from '../img/TICKET.png';
+import './navigation.css';
 
 const Nav = () => {
   return (
-    <nav>
-        <ul>
-            <li>
-                1
-            </li>
-            <li>
-                2
-            </li>
-            <li>
-                3
-            </li>
-            <li>
-                4
-            </li>
-        </ul>
+    <nav className='nav_bar'>
+        <NavLink to="/bids" className='navigate ' >
+            <img src={icon} alt='icon' className='icon navigate_icon' />
+        </NavLink>
+        
+        <NavLink to="/mine" className='navigate ' >
+            <img src={icon} alt='icon' className='icon navigate_icon' />
+        </NavLink>
+        
+        <NavLink to="/quests" className='navigate ' >
+            <img src={icon} alt='icon' className='icon navigate_icon' />
+        </NavLink>
+        
+        <NavLink to="/reff" className='navigate ' >
+            <img src={icon} alt='icon' className='icon navigate_icon' />
+        </NavLink>
+
     </nav>
   )
 }
