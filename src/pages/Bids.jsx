ffe24b139a0cab47577eb8d6d8d6d01b1c47c117
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-05-27 02:09:56 +0200

    html  pre bids page

--- src/pages/Bids.jsx
+++ src/pages/Bids.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import TON from "../img/TON.png";
 
 const Bids = () => {
   return (
     <section className='section bids_section'>
-      
+      <div className='price_block'>
+        <button className='info' >?</button>
+        <p className='text price_text'>toncoin price</p>
+        <h1 className='price_text text price'>20,69$</h1>
+      </div>
+      <div className='bids_block'>
+        <div className='bids_open'>
+          <button className='bid bid_up' >up</button>
+          <button className='bid bid_down'>down</button>
+        </div>
+        <div className='bids_value'>
+          <button className='bid_value 25%'>25%</button>
+          <button className='bid_value 50%'>50%</button>
+          <button className='bid_value 100%'>100%</button>
+          <div className='bid_value_input'>
+            <input type='number' className='bid_value input_value'/>
+            <img src={TON} alt="ton icon" />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
