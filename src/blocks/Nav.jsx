4907fe902d33301e8ac4b3fbab70ec5d06c352dd
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-05-29 03:22:51 +0200

    navbar v2
    navbar beter per static width, need update dynamic in future

--- src/blocks/Nav.jsx
+++ src/blocks/Nav.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 import home_icon from '../img/home.png';
 import comunity_icon from '../img/comunity.png';
 import mining_icon from '../img/mining.png';
@@ -7,25 +7,46 @@ import task_icon from '../img/task.png';
 import './navigation.css';
 
 const Nav = () => {
-  return (
-    <nav className='nav_bar'>
-        <NavLink to="/bids" className='navigate ' >
-            <img src={home_icon} alt='icon' className='icon navigate_icon' />
-        </NavLink>
-        
-        <NavLink to="/mine" className='navigate ' >
-            <img src={comunity_icon} alt='icon' className='icon navigate_icon' />
-        </NavLink>
-        
-        <NavLink to="/quests" className='navigate ' >
-            <img src={mining_icon} alt='icon' className='icon navigate_icon' />
-        </NavLink>
-        
-        <NavLink to="/reff" className='navigate ' >
-            <img src={task_icon} alt='icon' className='icon navigate_icon' />
-        </NavLink>
+    const navRef = useRef(null);
+    const location = useLocation();
 
-    </nav>
+    useEffect(() => {
+    const nav = navRef.current;
+    const active = nav.querySelector('.navigate.active');
+    if (nav && active) {
+        const navRect = nav.getBoundingClientRect();
+        const activeRect = active.getBoundingClientRect();
+
+        const cX = activeRect.left + activeRect.width / 2 - navRect.left - 2370;
+    
+       nav.style.setProperty('--hole-x', `${cX}px`);
+    }
+}, [location]);
+
+
+  return (
+    <>
+        
+        <nav className='nav_bar' ref={navRef}>
+            <div className='mask_nav_background'>
+                <NavLink to="/bids" className='navigate ' >
+                    <img src={home_icon} alt='icon' className='icon navigate_icon' />
+                </NavLink>
+                
+                <NavLink to="/mine" className='navigate ' >
+                    <img src={comunity_icon} alt='icon' className='icon navigate_icon' />
+                </NavLink>
+                
+                <NavLink to="/quests" className='navigate ' >
+                    <img src={mining_icon} alt='icon' className='icon navigate_icon' />
+                </NavLink>
+                
+                <NavLink to="/reff" className='navigate ' >
+                    <img src={task_icon} alt='icon' className='icon navigate_icon' />
+                </NavLink>
+            </div>
+        </nav>
+    </>
   )
 }
 
