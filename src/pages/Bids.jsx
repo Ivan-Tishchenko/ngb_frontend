cf5ecddeae43c6e4231c983436f4b1f7bc9c5c67
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-05-29 05:11:46 +0200

    bids page styles v0.5

--- src/pages/Bids.jsx
+++ src/pages/Bids.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import TON from "../img/TON.png";
+import "./bids.css";
 
 const Bids = () => {
   return (
@@ -9,20 +10,27 @@ const Bids = () => {
         <p className='text price_text'>toncoin price</p>
         <h1 className='price_text text price'>20,69$</h1>
       </div>
+
       <div className='bids_block'>
+
         <div className='bids_open'>
-          <button className='bid bid_up' >up</button>
-          <button className='bid bid_down'>down</button>
+          <button className='bid bid_up' ><span className='bid_symbol'>{"<"}</span>  up</button>
+          <button className='bid bid_down'><span className='bid_symbol'>{">"}</span>  down</button>
         </div>
+
         <div className='bids_value'>
+
           <button className='bid_value 25%'>25%</button>
           <button className='bid_value 50%'>50%</button>
           <button className='bid_value 100%'>100%</button>
+
           <div className='bid_value_input'>
             <input type='number' className='bid_value input_value'/>
             <img src={TON} alt="ton icon" />
           </div>
+
         </div>
+
       </div>
     </section>
   )
