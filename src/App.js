79c68927f882f04d25aa42de816f3dfc40fd0b82
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-06-01 00:48:18 +0200

    test 2 fix navbar with keyboard

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useEffect} from 'react';
 import User from 'blocks/User';
 import './App.css';
 import Nav from 'blocks/Nav';
@@ -9,8 +10,14 @@ import Refferals from 'pages/Refferals';
 // import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // const APP = "10.255.18.99:3000";
-
 function App() {
+
+  useEffect(() => {
+    if (window.Telegram?.WebApp) {
+        window.Telegram.WebApp.expand();
+    }
+  }, []);
+
   return (
     <>
     {/* <TonConnectUIProvider manifestUrl={`https://${APP}/tonconnect-manifest.json`}> */}
