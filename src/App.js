17e4f0d937be29af5ebb9c876a8a2711d145e92e
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-06-26 18:26:03 +0200

    test fix position navbar with opened keyboard

--- src/App.js
+++ src/App.js
@@ -13,6 +13,18 @@ import Refferals from 'pages/Refferals';
 function App() {
 
   useEffect(() => {
+    const setAppHeight = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+
+    setAppHeight();
+    
+
+    return 
+  }, []);
+
+  useEffect(() => {
     if (window.Telegram?.WebApp) {
         window.Telegram.WebApp.expand();
     }
