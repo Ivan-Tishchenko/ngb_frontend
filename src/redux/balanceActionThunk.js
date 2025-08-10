fe1cea0f432dfb918631702852e805225ae09dcf
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-08-10 18:56:10 +0200

    fix requests 2

--- src/redux/balanceActionThunk.js
+++ src/redux/balanceActionThunk.js
@@ -4,21 +4,21 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 const openBid = createAsyncThunk("user/setBid", async (bid,  { getState, rejectWithValue }) => {
     const user = getState();
     try {
-        const createdBid = await axios.post(`${process.env.REACT_APP_API_URL}api/bids/bid`, {
-                body: bid,
+        const createdBid = await axios.post(`${process.env.REACT_APP_API_URL}api/bids/bid`, bid,
                 headers: {
                     "Content-Type": "application/json",
                     // Authorization: `Bearer ${userData.token}` // если нужен токен
-                },
-            });
+                });
         if(createdBid.data) {
             const updateUser = await axios.post(`${process.env.REACT_APP_API_URL}api/users/update`, {
-                body: {
                     event: "bid",
                     bid: createdBid.data,
                     user: user,
-                }
-            })
+                }, 
+                 {
+                    "Content-Type": "application/json",
+                    // Authorization: `Bearer ${userData.token}` // если нужен токен
+                })
             return updateUser.data;
         }
     } catch (error) {
@@ -35,11 +35,13 @@ const closeBid = createAsyncThunk("user/closeBid", async (bidId, { getState, rej
         });
         if(bidInfo.data && bidInfo.data.result) {
            const updateUser = await axios.post(`${process.env.REACT_APP_API_URL}api/users/update`, {
-                body: {
                     event: "bid result",
                     bid: bidInfo.data,
                     user: user,
-                }});
+                }, {
+                    "Content-Type": "application/json",
+                    // Authorization: `Bearer ${userData.token}` // если нужен токен
+                });
             return {user: updateUser.data, bid: bidInfo.data };
         }
     } catch (error) {
