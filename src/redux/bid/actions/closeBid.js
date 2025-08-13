f1684a8982982e7a266a949795ed95d2bfe93c6d
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-08-13 17:46:49 +0200

    fix get request in getBid

--- src/redux/bid/actions/closeBid.js
+++ src/redux/bid/actions/closeBid.js
@@ -5,7 +5,7 @@ import closeBid_user from "../../user/actions/closeBid";
 const closeBid = createAsyncThunk("bid/closeBid", async (bidId, { dispatch, rejectWithValue }) => {
     try {
         const bidInfo = await axios.get(`${process.env.REACT_APP_API_URL}api/bids/bid`, {
-            params: {bidId}
+            params: {bidId: encodeURIComponent(bidId)}
         });
         
         if(bidInfo.data && bidInfo.data.result) {
