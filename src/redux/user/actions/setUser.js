ccf0c894d78bdaec8c5670dabbaa60e3f181078d
DUFENSHMIRTS-token <114497011+Ivan-Tishchenko@users.noreply.github.com>
2025-09-05 15:21:42 +0200

    fix route

--- src/redux/user/actions/setUser.js
+++ src/redux/user/actions/setUser.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import getBidData from 'redux/bid/actions/getBidData';
+import getBidData from '../../bid/actions/getBidData.js';
 
 
 
